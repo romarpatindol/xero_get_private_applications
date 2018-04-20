effcf99f65cc1e9bebf8619682ef7b338a152d32
Romar John Patindol <romarpatindol@gmail.com>
2018-04-20 18:25:22 +0800

    Remove try catch

--- main.py
+++ main.py
@@ -9,19 +9,13 @@ def myconverter(o):
         return o.__str__()
 
 def main(consumer_key, rsa_key, query):
-    try:
-        credentials = PrivateCredentials(consumer_key, rsa_key)
-        xero = Xero(credentials)
-        xero_data = eval("xero." + query)
+    credentials = PrivateCredentials(consumer_key, rsa_key)
+    xero = Xero(credentials)
+    xero_data = eval("xero." + query)
 
-        response = {
-            "data": xero_data
-        }
-    
-    except Exception as e:
-        response = {
-            "error": e
-        }
+    response = {
+        "data": xero_data
+    }
         
     # Convert datetime to str to make the data serializable
     str_response = json.dumps(response, default=myconverter)
